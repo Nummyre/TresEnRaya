aa7e45a193204866ece1e6386d0811b0622436c5
cintia <cintiia.349@gmail.com>
2019-06-04 18:35:07 +0200

    modificar la carpeta

--- src/tresenraya/TresEnRaya.java
+++ src/tresenraya/TresEnRaya.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package juego;
+package tresenraya;
 
 import java.awt.Color;
 import java.awt.Image;
@@ -102,6 +102,7 @@ public class TresEnRaya extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TresEnRaya.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
